2d498309384e2add832fe6edf4884ed6b9972ae6
MartinDuo <godplskillhim@gmail.com>
2021-03-14 14:14:30 +0100

    Adding USA

---
+++ USA.py
@@ -0,0 +1,12 @@
+# Search for channel "Da Real Money Getters"
+# subscribe to channel
+# view latest video
+# like
+# leave comment
+# share with facebook
+
+
+#proof
+#Youtube Username
+#Screenshot wathcing whole video, liked and subscribed
+#How often the word spoken in Video
